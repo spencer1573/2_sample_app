9772ab1353566c40cdc7721c0d8f2d0ecf3f2b70
Spencer D <snwmn24@gmail.com>
2015-09-16 20:53:31 +0000

    i was already done with exercise 3.6 q 1, and i just finished q 2 (q == question)

--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -9,11 +9,14 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
     assert_template 'static_pages/home'
     assert_select "a[href=?]", root_path, count: 2
     assert_select "a[href=?]", help_path
-    assert_select "a[href=?]", about_path
     assert_select "a[href=?]", contact_path
+    assert_select "a[href=?]", about_path
+    # for some reason this works now and it wasn't before. 
+    # i don't know what i did differently.
+    assert_select "a[href=?]", signup_path
+    #puts 
+    get signup_path
+    assert_select "title", "Sign up | Ruby on Rails Tut Sample App"
   end  
-  
-  
-  
   
 end

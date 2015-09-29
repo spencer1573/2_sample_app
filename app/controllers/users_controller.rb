e09bf4f1f92c14f9d5930d922e6f8c7fda4a5a2e
Spencer D <snwmn24@gmail.com>
2015-09-29 21:24:21 +0000

    added error messages when wrong things are submited to form. now on section 7.3.4

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,7 +12,8 @@ class UsersController < ApplicationController
   
   def create
     
-    @user = User.new(params[:user]) # not the final implementation!
+    @user = User.new(user_params) # not the final implementation!
+    
     if @user.save
       # Handle a successful save.
     else
@@ -20,5 +21,12 @@ class UsersController < ApplicationController
     end
     
   end
+  
+  private
+  
+    def user_params
+      params.require(:user).permit(:name, :email, :password,
+                                   :password_confirmation)
+    end
 
 end

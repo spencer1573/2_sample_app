bf4ab3f888d16dae036d362d410be19c35b39285
Spencer D <snwmn24@gmail.com>
2015-09-30 00:40:15 +0000

    error in rake test need to figure it out before you start 7.4 'Successful signups'

---
+++ test/integration/users_signup_test.rb
@@ -0,0 +1,26 @@
+require 'test_helper'
+
+class UsersSignupTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+  
+  test "invalid signup information" do
+    # remember this isn't necessary it just helps for
+    # readability and sometimes errors, talking about
+    # get signup_path
+    get signup_path
+  
+    assert_no_difference 'User.count' do 
+    
+      post user_path, user: { name: "",
+                              email: "user@invalid",
+                              password:               "foo",
+                              password_confirmation:  "bar" 
+      }
+    
+    end
+    assert_template 'users/new'
+  end
+  
+end

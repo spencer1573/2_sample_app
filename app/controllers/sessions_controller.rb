10dbf0c246cf6e7657508a5fbf467671d3fc877c
Spencer D <snwmn24@gmail.com>
2015-10-02 21:02:49 +0000

    just put in listing 8.5 under 8.1.3, need to investigate param with byebug, also learned how important it is not to just put get in routes.rb, updated so system works right with post and delete for creating and destorying a session. start with prying into params at listing 8.5 before 8.1.4

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,19 @@
 class SessionsController < ApplicationController
   def new
   end
+
+  def create
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      #log the user in and redirect to the user's show page.
+    else
+      # Create an error message.
+      flash[:danger] = 'Invalid email/password combination' #not quite right!
+      render 'new'
+    end
+  end
+
+  def destory
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,21 +1,24 @@
 Rails.application.routes.draw do
-  get 'sessions/new'
+  get     'sessions/new'
 
-  root             'static_pages#home'
+  root    'static_pages#home'
 # notice how important to distinguish
 # static_pages/help
 # vs
 # static_pages#help
 # i don't know why they are different. i will find out sometime. 
-  get 'help'    => 'static_pages#help'
-  get 'about'   => 'static_pages#about'
-  get 'contact' => 'static_pages#contact'
+  get     'help'    => 'static_pages#help'
+  get     'about'   => 'static_pages#about'
+  get     'contact' => 'static_pages#contact'
   
-  get 'signup'  => 'users#new'
-  
-  get 'login'   => 'sessions#new'
-  get 'login'   => 'sessions#create'
-  get 'logout'  => 'sessions#destroy'
+  get     'signup'  => 'users#new'
+
+# figured out how important it is that you don't always put get for routes
+# sometimes you put post and delete. for security we better make sure
+# we descriptify posts... i'm guessing.
+  get     'login'   => 'sessions#new'
+  post    'login'   => 'sessions#create'
+  delete  'logout'  => 'sessions#destroy'
   
   resources :users
 

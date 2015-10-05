7613a6132e2c79f95f65feba44ba6ebd4f0185c5
Spencer D <snwmn24@gmail.com>
2015-10-05 17:28:37 +0000

    generated integration test for sessions that makes sure the flash isn't displayed twice. it is currently failing. finished to 'Listing 8.8'

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,10 +6,12 @@ class SessionsController < ApplicationController
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
       #log the user in and redirect to the user's show page.
+      
     else
       # Create an error message.
       flash[:danger] = 'Invalid email/password combination' #not quite right!
       render 'new'
+      
     end
   end
 

5833375ca40ccb52fef9a28e9671639412d4a380
Spencer D <snwmn24@gmail.com>
2015-10-06 21:41:25 +0000

    added dropdown menu, and added an option for the mincost of the password to be used in the case of testing fixtures

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,4 +10,16 @@ class User < ActiveRecord::Base
   
   validates :password, presence: true, length: { minimum: 6 }
 
+  def User.digest(string)
+    # this is what i think is happening for thist first line
+    # its a ternary operator because of the question mark
+    # so 'ActiveModel::SecurePassword.min_cost' must melt down
+    # to a bool. and depending on that it decides to use
+    # the min cost, or the normal cost that the system is already
+    # set to.
+    cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
+                                                  BCrypt::Engine.cost
+    # and then this is pretty self explanitory.
+    BCrypt::Password.create(string, cost: cost)
+  end
 end

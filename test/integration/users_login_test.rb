4371497f3673064067afa8b3d1fc1c6c8c75319a
Spencer D <snwmn24@gmail.com>
2015-10-07 01:32:42 +0000

    added micheal test fixture, added setup method, and login with valid information test to UsersLoginTest

--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -4,6 +4,22 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
   # test "the truth" do
   #   assert true
   # end
+ 
+  def setup
+    @user = users(:michael)
+  end
+  
+  test "login with valid information" do 
+    get login_path
+    post login_path, session: { email: @user.email, password: 'password' }
+    assert_redirected_to @user # checks right "redirect target"
+    follow_redirect! # actually visit targeted page
+    assert_template 'users/show' # asserts show.html.erb
+    assert_select "a[href=?]", login_path, count: 0
+    assert_select "a[href=?]", logout_path
+    assert_select "a[href=?]", user_path(@user)
+  end
+
   
   test "login with invalid information" do
     get login_path

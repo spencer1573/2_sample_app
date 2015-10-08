806dcc5d7a1c49ad72b2d46d1436c19948f0611a
Spencer D <snwmn24@gmail.com>
2015-10-08 18:08:38 +0000

    added remember user after the user creates a new profile, and created the remember(user) method in the sessions helper

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,6 +7,7 @@ class SessionsController < ApplicationController
     if user && user.authenticate(params[:session][:password])
       #log the user in and redirect to the user's show page.
       log_in user
+      remember user
       redirect_to user #this is ~> user_url(user) <~
     else
       # Create an error message.
--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -6,6 +6,12 @@ module SessionsHelper
     session[:user_id] = user.id
   end
   
+  def remember(user)
+    user.remember
+    cookies.permanent.signed[:user_id] = user.id
+    cookies.permanent[:remember_token] = user.remember_token
+  end
+  
   #this makes it so you don't have to look up the
   # the user all the time. its the sort of thing you need
   # to implement in your noko_convert

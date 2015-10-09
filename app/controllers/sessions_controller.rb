5942324bda9701eb2b1b633d2470b3457e72285a
Spencer D <snwmn24@gmail.com>
2015-10-09 13:09:02 +0000

    added to the create method in the sessions controller so that the user would be remembered or forgotten bassed on if the user was clicked

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,8 +7,9 @@ class SessionsController < ApplicationController
     if user && user.authenticate(params[:session][:password])
       #log the user in and redirect to the user's show page.
       log_in user
-      remember user
-      redirect_to user #this is ~> user_url(user) <~
+      params[:session][:remember_me] == '1' ? remember(user) :
+                                              forget(user)
+      redirect_to user
     else
       # Create an error message.
       flash.now[:danger] = 'Invalid email/password combination' #not quite right!

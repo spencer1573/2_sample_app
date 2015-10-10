a17fd20caa39550db030645831e501cf11d29353
Spencer D <snwmn24@gmail.com>
2015-10-10 21:45:39 +0000

    finished up code for sessions_helper_test just need to add documentation and make sure i read everything below the code to make sure i understand it. this is for 'listing 8.55', also added raise that i think mentioned in a previous commit in the sessions_helper to make sure that a certain part of the code was being executed. i think test should not pass at this point but they soon will.

--- test/helpers/sessions_helper_test.rb
+++ test/helpers/sessions_helper_test.rb
@@ -3,3 +3,30 @@
 # test/test_helper.rb
 require 'test_helper'
 
+class SessionsHelperTest < ActionView::TestCase
+  
+  # "provides a setup method that you can override
+  # to define logic that runs before each test"
+  # -http://chriskottom.com/blog/2014/10/4-fantastic-ways-to-set-up-state-in-minitest/ 
+  # i'm guessing this is true about the testing
+  # system that is put in place here.
+  def setup
+    @user = users(:michael)
+    # i'm not sure which remember this is refering to
+    # i'm going to keep going and see if i find out. if i took a guess though
+    # i would say that its in
+    # app/controllers/helpers/sessions_helper.rb
+    remember(@user)
+  end
+  
+  test "current_user returns right user when session is nil" do
+    assert_equal @user, current_user
+    assert is_logged_in?
+  end
+  
+  test "current_user returns nil when remember digest is wrong" do
+    ### => see explanaitons and work out logic here when you come back
+    @user.update_attribute(:remember_digest, User.digest(User.new_token))
+    assert_nil current_user
+  end
+end

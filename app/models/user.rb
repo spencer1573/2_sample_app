c8b9e3642fae8818312a92e83b70a4d9894409ba
Spencer D <snwmn24@gmail.com>
2015-10-12 18:21:29 +0000

    finished 8.1 verified that both class << self, and User.new_token == self.new_token are the same thing.

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,21 +11,23 @@ class User < ActiveRecord::Base
   
   validates :password, presence: true, length: { minimum: 6 }
 
-  def User.digest(string)
-    # this is what i think is happening for thist first line
-    # its a ternary operator because of the question mark
-    # so 'ActiveModel::SecurePassword.min_cost' must melt down
-    # to a bool. and depending on that it decides to use
-    # the min cost, or the normal cost that the system is already
-    # set to.
-    cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
-                                                  BCrypt::Engine.cost
-    # and then this is pretty self explanitory.
-    BCrypt::Password.create(string, cost: cost)
-  end
-  
-  def User.new_token
-    SecureRandom.urlsafe_base64
+  class << self  
+    def digest(string)
+      # this is what i think is happening for thist first line
+      # its a ternary operator because of the question mark
+      # so 'ActiveModel::SecurePassword.min_cost' must melt down
+      # to a bool. and depending on that it decides to use
+      # the min cost, or the normal cost that the system is already
+      # set to.
+      cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
+                                                    BCrypt::Engine.cost
+      # and then this is pretty self explanitory.
+      BCrypt::Password.create(string, cost: cost)
+    end
+    
+    def new_token
+      SecureRandom.urlsafe_base64
+    end
   end
   
   def remember

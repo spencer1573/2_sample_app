ee7da599206a3614972be46e53f6ab411d84bc1e
Spencer D <snwmn24@gmail.com>
2015-10-19 23:39:12 +0000

    still going over simple test we made for pagination, just trying to make sure i understand everything

---
+++ test/integration/users_index_test.rb
@@ -0,0 +1,28 @@
+require 'test_helper'
+
+class UsersIndexTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+  
+  def setup
+    @user = users(:michael)
+  end
+  
+  test "index including pagination" do
+    log_in_as(@user)
+    # this users path comes from rescources i believe
+    get users_path
+    assert_template 'users/index'
+    assert_select 'div.pagination'
+    User.paginate(page: 1).each do |user|
+      # so this is saying there should be an html link, called by each
+      # user name that is linked to their user profile.
+      assert_select 'a[href=?]', user_path(user), text: user.name
+      
+    end
+    byebug
+  end
+  
+  
+end

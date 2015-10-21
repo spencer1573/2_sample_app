ec1e35ebbbac5ab22dd021c82ed8ac9d05469f76
Spencer D <snwmn24@gmail.com>
2015-10-21 12:32:10 +0000

    added documentation about modeling

--- db/migrate/20151021014433_add_admin_to_users.rb
+++ db/migrate/20151021014433_add_admin_to_users.rb
@@ -1,3 +1,5 @@
+# this is modeling well explained
+# https://gist.github.com/pyk/8569812
 class AddAdminToUsers < ActiveRecord::Migration
   def change
     add_column :users, :admin, :boolean, default: false

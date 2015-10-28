ffb6f0023e81a12f92e0d797c9df4b4ec3240609
Spencer D <snwmn24@gmail.com>
2015-10-28 18:26:04 +0000

    added last test to check pagination and links, finished chapter 9, user edit, update, index and destory actions complete

--- test/integration/users_index_test.rb
+++ test/integration/users_index_test.rb
@@ -6,22 +6,36 @@ class UsersIndexTest < ActionDispatch::IntegrationTest
   # end
   
   def setup
-    @user = users(:michael)
+    @admin     = users(:michael)
+    @non_admin = users(:archer)
   end
   
-  test "index including pagination" do
-    log_in_as(@user)
+  test "index as admin including pagination and delete links" do
+    log_in_as(@admin)
     # this users path comes from rescources i believe
     get users_path
     assert_template 'users/index'
     assert_select 'div.pagination'
-    User.paginate(page: 1).each do |user|
+    first_page_of_users = User.paginate(page: 1)
+    first_page_of_users.each do |user|
       # so this is saying there should be an html link, called by each
       # user name that is linked to their user profile.
       assert_select 'a[href=?]', user_path(user), text: user.name
+      unless user == @admin
+        assert_select 'a[href=?]', user_path(user), text: 'delete'
+      end
     end
-
+    # documentation found here:
+    # http://api.rubyonrails.org/classes/ActiveSupport/Testing/Assertions.html
+    assert_difference 'User.count', -1 do
+      delete user_path(@non_admin)
+    end
   end
   
+  test 'index as non-admin' do
+    log_in_as(@non_admin)
+    get users_path
+    assert_select 'a', text: 'delete', count: 0
+  end
   
 end

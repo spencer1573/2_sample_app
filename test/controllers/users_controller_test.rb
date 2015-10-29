dec07befb8c69ce5f2f2276449c0077bcc17e74b
Spencer D <snwmn24@gmail.com>
2015-10-29 19:46:39 +0000

    finished exercise 9.3

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -37,12 +37,21 @@ class UsersControllerTest < ActionController::TestCase
     assert_redirected_to root_url
   end
   
-  test "should redirect update when logged in as wrong user" do
+  test "should redirect update when logged in as wrong user" do 
     log_in_as(@other_user)
     patch :update, id: @user, user: { name: @user.name, email: @user.email }
-    assert flash.empty?
     assert_redirected_to root_url
   end
+  
+  test "should not allow the admin attribute to be edited via the web" do 
+    log_in_as(@other_user)
+    assert_not @other_user.admin?
+    patch :update, id: @other_user, user: { password:               "password",  
+                                            password_confirmation:  "password",  
+                                            admin: true }
+    assert_not @other_user.reload.admin?
+  end
+  
   
   test "should redirect index when not logged in" do 
     # this index refers to the users controller method.
@@ -68,5 +77,6 @@ class UsersControllerTest < ActionController::TestCase
     assert_redirected_to root_url
   
   end
+  
 
 end

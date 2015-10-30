7a2b782bf049dee7b90e313fb0654a2ca3760838
Spencer D <snwmn24@gmail.com>
2015-10-30 20:07:00 +0000

    added account activations resources for only the edit get path thing

--- config/routes.rb
+++ config/routes.rb
@@ -24,6 +24,7 @@ Rails.application.routes.draw do
   # is put in automatically and paired up so you don't have to 
   # manually do it.
   resources :users
+  resources :account_activations, only: [:edit]
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".

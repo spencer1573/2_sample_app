d17b8372bda7c985ab977107dc7e070dedf4812b
Spencer D <snwmn24@gmail.com>
2015-11-04 03:44:24 +0000

    added to the user model so that downcase email was a private method and create activation method digest method existed.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,14 @@
 class User < ActiveRecord::Base
   attr_accessor :remember_token, :activation_token
+  # that just means search for and do those methods
+  # before_save and 
+  # before_create
+  before_save   :downcase_email
+  before_create :create_activation_digest
+  
   # "we passed before_save an explicit block"
   # this site seems useful:
   # http://mudge.name/2011/01/26/passing-blocks-in-ruby-without-block.html
-  before_save { self.email = email.downcase }
   validates :name,  presence: true, length: { maximum: 50 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, length: { maximum: 255 },
@@ -44,5 +49,17 @@ class User < ActiveRecord::Base
   def forget
     update_attribute(:remember_digest, nil)
   end
+  
+  private
+    
+    # downcases email
+    def downcase_email
+      self.email = email.downcase
+    end
+    
+    def create_activation_digest
+      self.activation_token  = User.new_token
+      self.activation_digest = User.digest(activation_token)
+    end
   
 end

5706f65ee81487550a1372e7a54439e1da874185
Spencer D <snwmn24@gmail.com>
2015-11-18 19:57:26 +0000

    started writing account activations controller > edit method

--- app/controllers/account_activations_controller.rb
+++ app/controllers/account_activations_controller.rb
@@ -1,2 +1,9 @@
 class AccountActivationsController < ApplicationController
+  def edit 
+    user = User.find_by(email: params[:email])
+    user.update_attribute(:activated,     true)
+    user.update_attribute(:activated_at,  Time.zone.now)
+    
+    
+    
 end

2f0312367d8f5b98aac8cf5850cf573a7694e5ec
Spencer D <snwmn24@gmail.com>
2015-11-20 17:59:38 +0000

    added to edit method in accountactivations controller

--- app/controllers/account_activations_controller.rb
+++ app/controllers/account_activations_controller.rb
@@ -4,6 +4,7 @@ class AccountActivationsController < ApplicationController
     user.update_attribute(:activated,     true)
     user.update_attribute(:activated_at,  Time.zone.now)
     log_in user
+    flash[:success] = "Account activated"
     
     
 end

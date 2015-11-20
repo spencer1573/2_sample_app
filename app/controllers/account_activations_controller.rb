b413d6daf7fb080ba44e6a35f9a3eb45e320327c
Spencer D <snwmn24@gmail.com>
2015-11-20 00:09:02 +0000

    added to edit method in account activations controller

--- app/controllers/account_activations_controller.rb
+++ app/controllers/account_activations_controller.rb
@@ -3,7 +3,7 @@ class AccountActivationsController < ApplicationController
     user = User.find_by(email: params[:email])
     user.update_attribute(:activated,     true)
     user.update_attribute(:activated_at,  Time.zone.now)
-    
+    log_in user
     
     
 end

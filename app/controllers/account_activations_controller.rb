10416b1b1dca5ed28dd021d272a8b386badb426b
Spencer D <snwmn24@gmail.com>
2015-11-22 01:03:57 +0000

    finished the edit function in the account activations controller

--- app/controllers/account_activations_controller.rb
+++ app/controllers/account_activations_controller.rb
@@ -1,10 +1,18 @@
 class AccountActivationsController < ApplicationController
   def edit 
     user = User.find_by(email: params[:email])
-    user.update_attribute(:activated,     true)
-    user.update_attribute(:activated_at,  Time.zone.now)
-    log_in user
-    flash[:success] = "Account activated"
-    
-    
+    if user && !user.activated? && user.authenticated?(:activation, params[:id])  
+      user.update_attribute(:activated,     true)
+      user.update_attribute(:activated_at,  Time.zone.now)
+      log_in user
+      flash[:success] = "Account activated!"
+      redirect_to user
+    else
+      # this is put in place incase an activation link
+      # ends up in the wrong hands
+      # so it just links to the root_url
+      flash[:danger] = "Invalid activation link"
+      redirect_to root_url 
+    end
+  end
 end

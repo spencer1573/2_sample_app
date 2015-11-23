85fc0c21d090be1543c43cb3a683dbab359a7521
Spencer D <snwmn24@gmail.com>
2015-11-23 18:49:39 +0000

    added to create method conditionals in session controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,11 +5,15 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
-      #log the user in and redirect to the user's show page.
-      log_in user
-      params[:session][:remember_me] == '1' ? remember(user) : 
-                                              forget(user)
-      redirect_back_or user
+      if user.activated?
+        #log the user in and redirect to the user's show page.
+        log_in user
+        params[:session][:remember_me] == '1' ? remember(user) : 
+                                                forget(user)
+        redirect_back_or user
+      else
+        messge  = "Account not activated. "
+      end
     else
       # Create an error message.
       flash.now[:danger] = 'Invalid email/password combination' #not quite right!

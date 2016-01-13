07d3931a72d98d6a1c46a0657fde0252d9d34046
Spencer D <snwmn24@gmail.com>
2016-01-13 03:53:38 +0000

    started to make the create method in the password resetscontroller

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -2,6 +2,12 @@ class PasswordResetsController < ApplicationController
   def new
   end
 
+  def create
+    #i'm not exactly sure why email needs to be downcased.
+    @user = User.find_by(email: params[:password_reset][:email].downcase)
+  
+  end
+
   def edit
   end
 end

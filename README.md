06ac37d982c3ea5c66192aa6a913d662bd3f7467
Spencer D <snwmn24@gmail.com>
2016-01-14 03:22:28 +0000

    worked on the create method for password resets controller

--- README.md
+++ README.md
@@ -1,5 +1,9 @@
 == README
 
+# you are on listing 10.41
+
+
+
 This README would normally document whatever steps are necessary to get the
 application up and running.
 
--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -6,6 +6,10 @@ class PasswordResetsController < ApplicationController
     #i'm not exactly sure why email needs to be downcased.
     @user = User.find_by(email: params[:password_reset][:email].downcase)
   
+    if @user
+      # i looked ahead this gets built in the user model later
+      @user.create_reset_digest
+    
   end
 
   def edit

44eca7e81b655b2003cb7daa06bcd16b0cd8c12d
Spencer D <snwmn24@gmail.com>
2016-01-23 06:33:16 +0000

    added to the user mailer preview

--- test/mailers/previews/user_mailer_preview.rb
+++ test/mailers/previews/user_mailer_preview.rb
@@ -14,7 +14,13 @@ class UserMailerPreview < ActionMailer::Preview
   # http://localhost:3000/rails/mailers/user_mailer/password_reset
   # https://ubuntu-test-spencer1573.c9.io/rails/mailers/user_mailer/password_reset
   def password_reset
-    UserMailer.password_reset
+    user = User.first
+    # this reset_token is found on line 5 of the user model
+    # this new_token comes from line 38 of the user model i believe
+    # it looks like it just selects a random base 64 jumble of garbage numbers i thinkg 
+    user.reset_token = User.new_token
+    # this appears to be located in the app/mailers/user_mailer.rb
+    UserMailer.password_reset(user)
   end
 
 end

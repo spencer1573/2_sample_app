59eabd0db342b53e29ed46190a9522f1c956f438
spencer d <spencerkdavies@gmail.com>
2016-03-17 13:24:32 -0600

    finished invalid password, empty password and started valid password in password reset integration test

--- test/integration/password_resets_test.rb
+++ test/integration/password_resets_test.rb
@@ -90,8 +90,15 @@ class PasswordResetsTest < ActionDispatch::IntegrationTest
       email: user.email,
       user: { password:              "foobaz",
               password_confirmation: "barquux" }
-
-
+    assert_select 'div#error_explanation'
+    # EMPTY PASSWORD
+    patch password_reset_path(user.reset_token), 
+      email: user.email,
+      user: { password:              "",
+              password_confirmation: "" }
+    assert_select 'div#error_explanation'
+    # VALID PASSWORD & CONFIRMATION
+    patch password_reset_path(user.reset_token),
 
   end
 

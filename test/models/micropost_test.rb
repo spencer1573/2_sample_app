d2c9c970445e8038bd270222508f6d81448d41ea
Spencer <snwmn24@gmail.com>
2016-04-02 17:39:25 -0600

    started working on micropost test... it looks like when you go to edit things in the database they are automatically ignored. so i'm guessing i have to put a chef recipe in for putting things into production

--- test/models/micropost_test.rb
+++ test/models/micropost_test.rb
@@ -4,4 +4,8 @@ class MicropostTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+  def setup
+    @user = users(:michael)
+    # THIS CODE IS NOT IDIOMATICALLY CORRECT (so why am i writing it?)
+    @micropost = Micropost.new(content: "Lorem ipsum", user
 end

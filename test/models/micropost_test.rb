6456d29ea6c6d9ab2495b471149675e9d2b6148a
Spencer <snwmn24@gmail.com>
2016-04-04 10:56:01 -0600

    finished micropost_test testing for validity of micropost, and user id should be present

--- test/models/micropost_test.rb
+++ test/models/micropost_test.rb
@@ -7,5 +7,19 @@ class MicropostTest < ActiveSupport::TestCase
   def setup
     @user = users(:michael)
     # THIS CODE IS NOT IDIOMATICALLY CORRECT (so why am i writing it?)
-    @micropost = Micropost.new(content: "Lorem ipsum", user
+    @micropost = Micropost.new(content: "Lorem ipsum", user_id: @user.id)
+  end
+
+  test "should be valid" do
+    assert @micropost.valid?
+  end
+
+  test "user id should be present" do
+    #QUESTION i don't understand why you would set
+    #the micropost.user_id to nil, i don't see how that
+    #would test the if the id was present
+    @micropost.user_id = nil
+    assert_not @micropost.valid?
+  end
+    
 end

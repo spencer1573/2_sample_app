c14d575e5dcc107f0aa9098337c22c2d63cb8429
Spencer <snwmn24@gmail.com>
2016-04-05 17:11:40 -0600

    changed the micopost usermodel so it validated the presence of a user_id

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -1,3 +1,11 @@
 class Micropost < ActiveRecord::Base
+  # the belongs to was generated when 
+  # we typed 
+  # bundle exec rake db:migrate
   belongs_to :user
+  #QUESTION: i guess right now thats the only thing we 
+  # need to validate when a micropost is created 
+  # from the model?
+  #
+  validates :user_id, presence: true
 end

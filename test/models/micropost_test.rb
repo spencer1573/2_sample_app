db8f260b688a0fe5d8ffeb9a00621822e7eae0ec
Spencer <snwmn24@gmail.com>
2016-04-07 17:18:18 -0600

    added a test to make sure that content over 140 characters is not valid

--- test/models/micropost_test.rb
+++ test/models/micropost_test.rb
@@ -27,6 +27,9 @@ class MicropostTest < ActiveSupport::TestCase
     assert_not @micropost.valid?
   end
 
-
+  test "content should be at most 140 characters" do 
+    @micropost.content = "a" * 141
+    assert_not @micropost.valid?
+  end
     
 end

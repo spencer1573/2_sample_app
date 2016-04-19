bfdc24aa9ba60524b8d2c332e6b6a838d7858689
Spencer <snwmn24@gmail.com>
2016-04-19 17:15:51 -0600

    fixed one of the tests by taking the out of it so i could target it more easily it was testing the order of the microposts

--- test/models/micropost_test.rb
+++ test/models/micropost_test.rb
@@ -31,7 +31,7 @@ class MicropostTest < ActiveSupport::TestCase
     assert_not @micropost.valid?
   end
     
-  test "order should be the most recent first" do
+  test "order should be most recent first" do
     assert_equal microposts(:most_recent), Micropost.first
   end
 end

b9ff23f0ff81bbc589ff99595bb565042baf7bfd
Spencer <snwmn24@gmail.com>
2016-04-30 18:22:19 -0600

    added question about pagination

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -22,6 +22,7 @@ class UsersController < ApplicationController
   
   def show
     @user = User.find(params[:id])
+    #QUESTION i don't really understand why the params are the page
     @microposts = @user.microposts.paginate(page: params[:page])
   end
 
